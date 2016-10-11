95df87436b7fd26ce5429379dca973490840a95a
Drew Juengel <Drew Juengel>
2016-10-10 18:29:23 -0600

    end of Monday

--- Final/public/js/module.dashboard.js
+++ Final/public/js/module.dashboard.js
@@ -1,18 +1,20 @@
-angular.module('module.dashboard', ['ngRoute'])
+angular.module('Pam', ['ngRoute'])
     .controller('DashboardController', dashboard)
     .factory('artificialFactory', aiFactory);
 
-dashboard.$inject=['$scope', 'aiFactory'];
+dashboard.$inject=['$scope', 'artificialFactory'];
 aiFactory.$inject=['$http'];
 
-function dashboard($scope, aiFactory) {
-    console.info('Dashboard initialized');
+function dashboard($scope, artificialFactory) {
+    console.info('dashboard controller initialized');
     var dCtrl = this;
 }
 
 function aiFactory($http){
     console.log('Factory is running..')
     var factoryObj = {}
+
+        
 
     return factoryObj;
 }
